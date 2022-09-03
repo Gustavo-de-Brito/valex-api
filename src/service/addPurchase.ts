f5e5530e1cf455f66c46b69f8969553dd3e281a6
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-09-03 18:33:29 -0300

    feat: add new purchase to the database if all the datas are valid

--- src/service/addPurchase.ts
+++ src/service/addPurchase.ts
@@ -1,7 +1,10 @@
 import dayjs from 'dayjs';
-import * as cardRepository from '../repositories/cardRepository';
 import bcrypt from 'bcrypt';
+import * as cardRepository from '../repositories/cardRepository';
+import * as businessRepository from '../repositories/businessRepository';
+import * as paymentRepository from '../repositories/paymentRepository';
 
+// card validation functions
 async function isCardValid(cardId: number) {
   const cardData = await cardRepository.findById(cardId);
 
@@ -55,17 +58,59 @@ function isPasswordValid(databasePassword: string, password: string) {
   }
 }
 
+async function doesCardHaveBalance(cardId:number, purchaseAmount: number) {
+  const employeeBalance:any = await paymentRepository.getBalance(cardId);
+
+  if(purchaseAmount > employeeBalance.balance) {
+    throw {
+      code: 'Unprocessable',
+      message: 'Não há crédito suficiente para realizar a compra'
+    };
+  }
+}
+
+// business validation functions
+async function isBusinessRegistered(businessId: number) {
+  const businessData = await businessRepository.findById(businessId);
+
+  if(!businessData) {
+    throw {
+      code: 'NotFound',
+      message: 'Este comércio não está registrado'
+    };
+  }
+
+  return businessData;
+}
+
+function sameCardBusinessType(cardType: string, businessType: string) {
+  if(cardType !== businessType) {
+    throw {
+      code: 'Unauthorized',
+      message: 'Este cartão não pode ser usado nesse tipo de estabelecimento'
+    };
+  }
+}
+
 async function addPurchase(
   cardId: number,
   password: string,
   businessId: number,
   amount: number
 ) {
+  // card validations
   const cardData:any = await isCardValid(cardId);
   isCardActiveded(cardData.password);
   isExpirationDateValid(cardData.expirationDate);
   isCardBlocked(cardData.isBlocked);
   isPasswordValid(cardData.password, password);
+  await doesCardHaveBalance(cardData.id, amount);
+
+  // business validations
+  const businessData: any = await isBusinessRegistered(businessId);
+  sameCardBusinessType(cardData.type, businessData.type);
+
+  await paymentRepository.insert({ cardId, businessId, amount });
 }
 
 export default addPurchase;

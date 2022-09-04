3c6814e7498ddf4d290076e3b606248c777258b0
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-09-04 15:59:10 -0300

    feat: validate request card data with the database

--- src/constrollers/permissionController.ts
+++ src/constrollers/permissionController.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from 'express';
+import getCardBlocked from '../service/getCardBlocked';
 
 export async function blockCard(req: Request, res: Response) {
+  const { cardId, password }:{ cardId: number, password: string } = req.body;
+
   try {
+    await getCardBlocked(cardId, password);
+
     res.sendStatus(200);
   } catch(err:any) {
+    if(err.code === 'Unauthorized') return res.status(401).send(err.message);
+    else if(err.code === 'Unprocessable') return res.status(422).send(err.message);
+
     res.sendStatus(500);
   }
 }
---
+++ src/service/getCardBlocked.ts
@@ -0,0 +1,57 @@
+import dayjs from 'dayjs';
+import bcrypt from 'bcrypt';
+import * as cardRepository from '../repositories/cardRepository';
+
+async function isCardValid(cardId: number) {
+  const cardData = await cardRepository.findById(cardId);
+
+  if(!cardData) {
+    throw {
+      code: 'Unauthorized',
+      message: 'Os dados do cartão estão incorretos'
+    };
+  }
+
+  return cardData;
+}
+
+function isExpirationDateValid(expirationDate: string) {
+  const currentDate = dayjs().format('MM/YY');
+
+  if(currentDate > expirationDate) {
+    throw {
+      code: 'Unauthorized',
+      message: 'Esse cartão passou do prazo de validade'
+    };
+  }
+}
+
+function isCardBlocked(isBlocked: boolean) {
+  if(isBlocked) {
+    throw {
+      code: 'Unprocessable',
+      message: 'Esse cartão já está bloqueado'
+    };
+  }
+}
+
+function isPasswordValid(databasePassword: string, password: string) {
+  const isValidPassword = bcrypt.compareSync(password, databasePassword ?? '');
+
+  if(!isValidPassword) {
+    throw {
+      code: 'Unauthorized',
+      message: 'Os dados do cartão estão incorretos'
+    };
+  }
+}
+
+async function getCardBlocked(cardId: number, password: string) {
+  // card validations
+  const cardData:any = await isCardValid(cardId);
+  isExpirationDateValid(cardData.expirationDate);
+  isCardBlocked(cardData.isBlocked);
+  isPasswordValid(cardData.password, password);
+}
+
+export default getCardBlocked;
